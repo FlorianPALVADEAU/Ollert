5f5399eecdccb9d75b2df424ab7d8a965983cd13
Louis Perrenot <perrenotlouis1804@gmail.com>
2025-04-01 09:44:29 +0200

    Add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
 import { Providers } from "./providers";
 import "./globals.css";
+import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {AppSidebar} from "@/components/app-sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html 
+    <html
       lang="FR"
       title="We 💘 pepe!"
     >
       <body>
         <Providers>
-          {children}
+          <SidebarProvider>
+            <AppSidebar />
+            <main className="w-full">
+              <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <SidebarTrigger />
+              </header>
+              {children}
+            </main>
+          </SidebarProvider>
         </Providers>
       </body>
     </html>
